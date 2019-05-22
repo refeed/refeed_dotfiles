073d680107fbbc8eba575f7a962fae246af09c3f
Rafid Aslam <rafidteam@gmail.com>
2019-05-22 14:54:03 +0700

    Add clone_ohmyzsh_plugins.sh

---
+++ clone_ohmyzsh_plugins.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
